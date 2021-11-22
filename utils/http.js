a270b797b5f725d8138d59581e782c6199f6c951
liuballoon <liuballoon@163.com>
2021-11-22 22:32:34 +0800

    build: http封装类

---
+++ utils/http.js
@@ -0,0 +1,21 @@
+import { config } from "../core/config"
+import { makePromise } from "./make-promise"
+
+/**
+ * 封装HTTP
+ */
+class Http {
+
+  static async request({ url, data, method = 'GET' }) {
+    const res = await makePromise(wx.request)({
+      url: `${config.baseApi}${url}`,
+      data,
+      method
+    })
+    return res.data.data
+  }
+}
+
+export {
+  Http
+}
