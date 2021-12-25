eee77b8d8b5b5026289fdc8c1ab58101fd3bbf4e
liuballoon <liuballoon@163.com>
2021-12-25 21:49:23 +0800

    build: 封装价格组件

---
+++ components/price/index.js
@@ -0,0 +1,19 @@
+// components/price/index.js
+Component({
+
+  properties: {
+    value: String,
+    valueSize: {
+      type: String,
+      value: 32
+    }
+  },
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+})
