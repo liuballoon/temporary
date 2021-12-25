fbfe51693db1c1e45390a34d0f74d614a40c889f
liuballoon <liuballoon@163.com>
2021-12-25 21:49:23 +0800

    build: 商品预览组件

---
+++ components/spu-preview/index.js
@@ -0,0 +1,36 @@
+// components/spu-preview/index.js
+Component({
+
+  properties: {
+    data: Object
+  },
+
+  observers: {
+    'data': function (data) {
+      // console.log(data)
+      if (!data) {
+        return
+      }
+      if (!data.tags) {
+        return
+      }
+      const tags = data.tags.split('#')
+      this.setData({
+        tags
+      })
+    }
+  },
+
+  data: {
+    tags: []
+  },
+
+  methods: {
+    onTapSpu(event) {
+      const spuId = event.currentTarget.dataset.id
+      wx.navigateTo({
+        url: `/pages/spu-detail/spu-detail?spuId=${spuId}`,
+      })
+    }
+  }
+})
