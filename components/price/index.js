ecc854e6ac3374d02d56bf6ffe22ad3e152f53a9
liuballoon <liuballoon@163.com>
2022-06-27 15:12:04 +0800

    build: 构建价格组件

--- components/price/index.js
+++ components/price/index.js
@@ -2,15 +2,25 @@
 Component({
 
   properties: {
-    value: String,
-    valueSize: {
-      type: String,
-      value: 32
+    value: String
+  },
+
+  observers: {
+    'value': function (value) {
+      if (!value) {
+        return
+      }
+      const arr = value.split('.')
+      this.setData({
+        amount: arr[0],
+        decimal: arr.length == 2 ? arr[1] : ''
+      })
     }
   },
 
   data: {
-
+    amount: '00',
+    decimal: ''
   },
 
   methods: {
